744f93a42df818ff9c98a5434329abeb5881b9b6
momanhaes <momanhaes@gmail.com>
2020-04-13 23:35:50 -0300

    fix awesome

--- js/accordion.js
+++ js/accordion.js
@@ -7,7 +7,6 @@ $(".accordion").click(function (e) {
   if ($.inArray(name, headers) > -1) {
     var subItem = $(target).next();
 
-    //slideUp all elements (except target) at current depth or greater
     var depth = $(subItem).parents().length;
     var allAtDepth = $(".accordion p, .accordion div").filter(function () {
       if ($(this).parents().length >= depth && this !== subItem.get(0)) {
@@ -16,7 +15,6 @@ $(".accordion").click(function (e) {
     });
     $(allAtDepth).slideUp("fast");
 
-    //slideToggle target content and adjust bottom border if necessary
     subItem.slideToggle("fast", function () {
       $(".accordion :visible:last").css("border-radius", "0 0 10px 10px");
     });
