d6f6aec809ae8ed303542997c0262e3c162ca2a2
momanhaes <momanhaes@gmail.com>
2020-04-21 10:40:39 -0300

    feat: Area da agenda na home

--- js/backtotop.js
+++ js/backtotop.js
@@ -6,6 +6,6 @@ $(window).scroll(function () {
   }
 });
 $(".back-to-top").click(function () {
-  $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+  $("html, body").animate({ scrollTop: 0 }, 100, "easeInOutExpo");
   return false;
 });
---
+++ js/routes.js
@@ -0,0 +1,6 @@
+// const express = require("express");
+// const routes = express.Router();
+
+// routes.get("/home", express.static("index.html"));
+
+// module.exports = routes;
