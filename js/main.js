31e8168fda26679da9009629dfaa18d5ecc6ab15
momanhaes <momanhaes@gmail.com>
2020-05-03 07:13:31 -0300

    feat: Insere sweetalert

---
+++ js/main.js
@@ -0,0 +1,44 @@
+function ebd() {
+  Swal.fire({
+    title: "<h4><strong>Informativo Escola Bíblica<strong></h4>",
+    background: "#cecece",
+    confirmButtonColor: "#3f5eac",
+    confirmButtonText: "Entendi",
+    html: `<p style="line-height: 1.5rem;">Nossa escola bíblica continua de forma online.</p> 
+
+    <p style="line-height: 1.5rem;">Nossos professores não estão medindo esforços para fazer com que o estudo da Palavra de Deus chegue até você através de vídeos, 
+    lives, áudios, interação em grupos de WhatsApp, etc.</p> 
+    
+    <p style="line-height: 1.5rem;">Entre em contato com seu professor e não perca essa oportunidade.</p> 
+    <p style="line-height: 1.5rem;">O Espírito  continua  soprando, vamos  levantar  as  velas!</p>`,
+  });
+}
+
+function igp() {
+  Swal.fire({
+    title: "<h4><strong>Informativo Igreja de Gente Pequena<strong></h4>",
+    background: "#cecece",
+    confirmButtonColor: "#3f5eac",
+    confirmButtonText: "Entendi",
+    html: `<p style="line-height: 1.5rem;">Nesse período que devemos permanecer em casa não queremos deixar de alimentar as nossas crianças.</p>
+
+    <p style="line-height: 1.5rem;">Temos algumas formas de fazer isso:</p>
+
+    <p style="line-height: 1.5rem;"><strong>1.</strong> Material semanal para trabalho dos pais com as crianças, ou seja, os pais serão os professores.</p> 
+    
+    <p style="line-height: 1.5rem;"><strong>2.</strong> Vamos tentar enviar vídeos feitos pelos professores da IGP contando algumas histórias bíblicas e sugerindo atividades. 
+    Quem tem possibilidade de imprimir em casa, podemos encaminhar uma atividade escrita referente ao vídeo.</p>
+
+    <p style="line-height: 1.5rem;">Caso você queira receber esse tipo de material, envie uma mensagem no <i class="fa fa-whatsapp"></i> 
+    
+    WhatsApp para a Rebecca Lima <i class="fa fa-hand-o-down"></i> <p style="line-height: 1.5rem;"><strong>(21) 9 7952-1818</strong></p></p>
+  
+    <p style="line-height: 1.5rem;"><strong>3.</strong> As crianças têm acesso direto e às vezes irrestrito a internet.</p> 
+    
+    <p style="line-height: 1.5rem;">Por favor, cuidado! Vigie do que seu filho está se alimentando.</p>  
+  
+    <p style="line-height: 1.5rem;">Sugerimos um aplicativo que conta histórias bíblicas e tem atividades. Clique no link abaixo para acessar <i class="fa fa-hand-o-down"></i></p> 
+  
+    <p style="line-height: 1.5rem;"><a href="https://apps.apple.com/br/app/app-dab%C3%ADblia-para-crian%C3%A7as/id668692393">App da Bíblia para Crianças da Life.Church</a></p>`,
+  });
+}
