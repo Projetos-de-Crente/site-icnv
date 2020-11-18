c50b113756643414b1867b663a60247805af4b9d
momanhaes <momanhaes@gmail.com>
2020-11-18 17:42:07 -0300

    feat: Insere bootstrap, fontawesome e sweet alert ao projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'site-icnv';
+export class AppComponent implements OnInit {
+  ngOnInit() {}
 }
